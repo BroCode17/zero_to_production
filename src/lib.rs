eb0a8fec15668488a3f01283c21aefc0d1fa1096
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-12-18 00:31:44 -0500

    handle post request with TTD

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,31 @@
 use std::io;
 
-use actix_web::{web::{self,}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web::{self, Form,}, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData{
+    email: String,
+    name: String
+}
 
 async fn health_checker() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
 
-pub fn run() -> Result<Server, std::io::Error> {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_checker))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("127.0.0.1:8000")? // error occurs quit
+    .listen(listener)? // error occurs quit
     .run();
     
     Ok(server)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::net::TcpListener;
+
 use zero_to_production::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on the server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Field to bind");
+    //  run(TcpListener::bind(listener))?.await
+    Ok(())
 }
